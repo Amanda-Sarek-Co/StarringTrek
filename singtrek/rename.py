0df9a345d2120eeb2322fa6f8e933fc57df4c214
Halle Countryman <hkcountryman@gmail.com>
2020-12-12 16:59:55 -0800

    added getCharacter function

--- singtrek/rename.py
+++ singtrek/rename.py
@@ -5,9 +5,9 @@ from pprint import pprint
 from operator import itemgetter
 
 def getPerformer(performer_name="Nana Visitor"):
-    criteria = stapi.search_criteria.PerformerSearchCriteria(0,1,"", name="Nana Visitor") 
+    criteria = stapi.search_criteria.PerformerSearchCriteria(0,1,"",name="Nana Visitor") 
     response = stapi.RestClient().performer.search(criteria) # this returns a dictonary 
-    performers = response['performers']
+    performers = response["performers"]
     
     if len(performers) == 1:
         performer = performers[0]
@@ -17,8 +17,8 @@ def getPerformer(performer_name="Nana Visitor"):
     rest_client = stapi.RestClient()
     return rest_client.performer.get(performer["uid"])
 
-def getEpisode(episode_name="The Storyteller"):
-    criteria = stapi.search_criteria.EpisodeSearchCriteria(0,1,'', title="The Storyteller")
+def getEpisode(episode_name = "The Storyteller"):
+    criteria = stapi.search_criteria.EpisodeSearchCriteria(0,1,"",title="The Storyteller")
     response = stapi.RestClient().episode.search(criteria)
     episodes = response["episodes"]
     episode = episodes[0]
@@ -26,3 +26,16 @@ def getEpisode(episode_name="The Storyteller"):
     rest_client = stapi.RestClient()
 
     return rest_client.episode.get(episode["uid"])
+
+def getCharacter(character_name = "Kira Nerys"):
+    criteria = stapi.search_criteria.CharacterSearchCriteria(0,1,"",name="Kira Nerys")
+    response = stapi.RestClient().character.search(criteria)
+    characters = response["characters"]
+    if len(characters) == 1:
+        character = characters[0]
+    else:
+        character = characters[0]   # change so it includes entries besides the first
+
+    rest_client = stapi.RestClient()
+    
+    return rest_client.character.get(character["uid"])
